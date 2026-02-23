b19af236dff9e622fbe6c805f482c7c4156f7fc6
Vishwanath Mishra <vishwanathmishra2804@gmail.com>
2026-02-24 00:44:42 +0530

    implemented code fixes

--- components/Upload.tsx
+++ components/Upload.tsx
@@ -122,7 +122,7 @@ const Upload = ({ onComplete }: UploadProps) => {
                                 "Click to upload or just drag and drop"
                             ): ("Sign in or sign up with Puter to upload")}
                         </p>
-                        <p className="help">Maximum file size 50 MB.</p>
+                        <p className="help">Maximum file size 10 MB.</p>
                     </div>
                 </div>
             ) : (
